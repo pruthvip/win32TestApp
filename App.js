9137eac5675b4100ed44864fbd08bc533a207e25
Pruthvi Prakasha <pruthvip@microsoft.com>
2021-04-08 12:48:20 +0530

    Basic win32 project

---
+++ App.js
@@ -0,0 +1,64 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+
+const App = () => {
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <View>
+        <Text>
+          jsdklsjdf skfjsdklf sflks dfjsf
+        </Text>
+        <Text>
+          jsdklsjdf skfjsdklf sflks dfjsf
+        </Text>
+        <Text>
+          jsdklsjdf skfjsdklf sflks dfjsf
+        </Text>
+        <Text>
+          jsdklsjdf skfjsdklf sflks dfjsf
+        </Text>
+        <Text>
+          jsdklsjdf skfjsdklf sflks dfjsf
+        </Text>
+        <Text>
+          jsdklsjdf skfjsdklf sflks dfjsf
+        </Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
